cbb181040807aa3f8a2806c91292eb0f972ea8a4
JemmyChen0221 <114365434+JemmyChen0221@users.noreply.github.com>
2023-12-07 18:39:10 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent the form from actually submitting
+    alert('Thank you for your message! We will get back to you soon.');
+});
